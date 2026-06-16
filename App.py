922777bc07dc82d5c0322abbbe623ea5ece52fa0
ocxacell <lemussebastian843@gmail.com>
2026-06-15 23:32:30 -0500

    Test: Se prueban las def hasta ahora desarrolladas

--- App.py
+++ App.py
@@ -1,14 +1,18 @@
 import pandas as pd
 from Src.Extraccion_de_datos import extraer_csv, extraer_selecciones_jugadores
 from Src.Creacion_de_objetos import crear_selecciones
+from Src.Creacion_de_objetos import crear_jugadores
+from Src.Transformaciones_de_datos import NacimientoAedad
+
 from Modelos.Clases import Seleccion
 #Paso 1: Extraer daots
 
 df_selecciones = extraer_csv('Data/SquadLists.csv')
-
+df_selecciones = NacimientoAedad(df_selecciones)
+df_selecciones.head()
 Selecciones = crear_selecciones(df_selecciones)
-print(Selecciones['Mexico'])
+Selecciones = crear_jugadores(df_selecciones, Selecciones)
+print(Selecciones['Mexico'].jugadores[0])
 
-jugadores_por_seleccion, selecciones = extraer_selecciones_jugadores(df_selecciones)
 
 #Crear objeto seleccion y jugador 
