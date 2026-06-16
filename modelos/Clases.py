f54ea68b599c3b8a9ad4b1c63612385f92666e6f
ocxacell <lemussebastian843@gmail.com>
2026-06-15 23:30:00 -0500

    feat: Se crea funcion en la clase seleccion para anadir jugadores

--- modelos/Clases.py
+++ modelos/Clases.py
@@ -1,13 +1,14 @@
 from dataclasses import dataclass,field
 
 @dataclass
-class Seleccion:
-    nombre: str
-    jugadores: list = field(default_factory=list)
-
-@dataclass
 class Jugador: 
     nombre: str
     posicion: str
     edad: int
     
+@dataclass
+class Seleccion:
+    nombre: str
+    jugadores: list = field(default_factory=list)
+    def agregar_jugador(self, jugador: Jugador):
+        self.jugadores.append(jugador)
