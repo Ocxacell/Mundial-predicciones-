33951c916a5b15c4ebba76c671d946b58c3a3b48
ocxacell <lemussebastian843@gmail.com>
2026-06-15 23:24:30 -0500

    Se crea funcion interna en la clase seleccion para anadir jugadores

--- Src/Creacion_de_objetos.py
+++ Src/Creacion_de_objetos.py
@@ -6,3 +6,9 @@ def crear_selecciones(df):
     for nombre in df["Team"].unique():
         selecciones[nombre] = Seleccion(nombre)
     return selecciones
+
+def crear_jugadores(df, selecciones):
+    for _,row in df.iterrows():
+        jugador =  Jugador(nombre=row['Player Name'], posicion=row['Position'], edad=row['Age'])
+        selecciones[row['Team']].agregar_jugador(jugador)
+    return selecciones
