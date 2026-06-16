a1568e395d338e6db1bd0e8bf038b9c6d5f2e1d5
ocxacell <lemussebastian843@gmail.com>
2026-06-15 23:27:12 -0500

    fix: Se agrega return a la funcion de lectura de csv

--- Src/Extraccion_de_datos.py
+++ Src/Extraccion_de_datos.py
@@ -4,3 +4,14 @@ import pandas as pd
 
 def extraer_csv(ruta_archivo):
     df = pd.read_csv(ruta_archivo)
+    return df
+
+def extraer_selecciones_jugadores(df):
+    selecciones = df['Team'].unique()
+    jugadores_por_seleccion = {}
+    
+    for seleccion in selecciones:
+        jugadores = df[df['Team'] == seleccion]['Player Name'].tolist()
+        jugadores_por_seleccion[seleccion] = jugadores
+        
+    return jugadores_por_seleccion,selecciones
